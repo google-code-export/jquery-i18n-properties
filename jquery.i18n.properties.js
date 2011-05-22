e46853b67b3e70bd765082d1d0d2bb8474dcb049
nfgrilo@gmail.com <nfgrilo@gmail.com@60ed876c-c37a-d86c-05f3-22939075770b>
2011-05-22 11:12:59 +0000

    Initial commit: transferred existing tagged code from codingwithcoffee.com

--- jquery.i18n.properties.js
+++ jquery.i18n.properties.js
@@ -17,7 +17,7 @@ $.i18n = {};
 
 /** Map holding bundle keys (if mode: 'map') */
 $.i18n.map = {};
-    
+
 /**
  * Load and parse message bundle files (.properties),
  * making bundles keys available as javascript variables.
@@ -49,12 +49,12 @@ $.i18n.properties = function(settings) {
         name:           'Messages',
         language:       '',
         path:           '',  
-        mode:           'vars',
+        mode:           'vars',       
         callback:       function(){}
     };
     settings = $.extend(defaults, settings);    
     if(settings.language === null || settings.language == '') {
-	   settings.language = $.i18n.browserLang();
+	   settings.language = $.i18n.browserLang();//normaliseLanguageCode(navigator.language /* Mozilla */ || navigator.userLanguage /* IE */);
 	}
 	if(settings.language === null) {settings.language='';}
 	
@@ -105,94 +105,83 @@ $.i18n.browserLang = function() {
 
 /** Load and parse .properties files */
 function loadAndParseFile(filename, language, mode) {
-	$.ajax({
+    $.ajax({
         url:        filename,
         async:      false,
-        cache:		false,
-        contentType:'text/plain;charset=UTF-8',
+        contentType: "text/plain;charset=UTF-8",
         dataType:   'text',
         success:    function(data, status) {
-        				parseData(data, mode); 
-					}
-    });
-}
-
-/** Parse .properties files */
-function parseData(data, mode) {
-   var parsed = '';
-   var parameters = data.split( /\n/ );
-   var regPlaceHolder = /(\{\d+\})/g;
-   var regRepPlaceHolder = /\{(\d+)\}/g;
-   var unicodeRE = /(\\u.{4})/ig;
-   for(var i=0; i<parameters.length; i++ ) {
-       parameters[i] = parameters[i].replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim
-       if(parameters[i].length > 0 && parameters[i].match("^#")!="#") { // skip comments
-           var pair = parameters[i].split('=');
-           if(pair.length > 0) {
-               /** Process key & value */
-               var name = unescape(pair[0]).replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim
-               var value = pair.length == 1 ? "" : pair[1];
-               // process multi-line values
-               while(value.match(/\\$/)=="\\") {
-               		value = value.substring(0, value.length - 1);
-               		value += parameters[++i].replace( /\s\s*$/, '' ); // right trim
-               }               
-               for(var s=2;s<pair.length;s++){ value +='=' + pair[s]; }
-               value = value.replace( /"/g, '\\"' ); // escape quotation mark (")
-               value = value.replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim  
-               
-               /** Mode: bundle keys in a map */
-               if(mode == 'map' || mode == 'both') {
-                   // handle unicode chars possibly left out
-                   var unicodeMatches = value.match(unicodeRE);
-                   if(unicodeMatches) {
-                     for(var u=0; u<unicodeMatches.length; u++) {
-                        value = value.replace( unicodeMatches[u], unescapeUnicode(unicodeMatches[u]));
-                     }
-                   }
-                   // add to map
-                   $.i18n.map[name] = value;
-               }
-               
-               /** Mode: bundle keys as vars/functions */
-               if(mode == 'vars' || mode == 'both') {
-                   // make sure namespaced key exists (eg, 'some.key') 
-                   checkKeyNamespace(name);
-                   
-                   // value with variable substitutions
-                   if(regPlaceHolder.test(value)) {
-                       var parts = value.split(regPlaceHolder);
-                       // process function args
-                       var first = true;
-                       var fnArgs = '';
-                       var usedArgs = [];
-                       for(var p=0; p<parts.length; p++) {
-                           if(regPlaceHolder.test(parts[p]) && (usedArgs.length == 0 || usedArgs.indexOf(parts[p]) == -1)) {
-                               if(!first) {fnArgs += ',';}
-                               fnArgs += parts[p].replace(regRepPlaceHolder, 'v$1');
-                               usedArgs.push(parts[p]);
-                               first = false;
+                       var parsed = '';
+                       var parameters = data.split( /\n/ );
+                       var regPlaceHolder = /(\{\d+\})/g;
+                       var regRepPlaceHolder = /\{(\d+)\}/g;
+                       var unicodeRE = /(\\u.{4})/ig;
+                       for(var i=0; i<parameters.length; i++ ) {
+                           parameters[i] = parameters[i].replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim
+                           if(parameters[i].length > 0 && parameters[i].match("^#")!="#") { // skip comments
+                               var pair = parameters[i].split('=');
+                               if(pair.length > 0) {
+                                   /** Process key & value */
+                                   var name = unescape(pair[0]).replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim
+                                   var value = pair.length == 1 ? "" : pair[1];
+                                   for(var s=2;s<pair.length;s++){ value +='=' + pair[s]; }
+                                   value = value.replace( /"/g, '\\"' ); // escape quotation mark (")
+                                   value = value.replace( /^\s\s*/, '' ).replace( /\s\s*$/, '' ); // trim  
+                                   
+                                   /** Mode: bundle keys in a map */
+                                   if(mode == 'map' || mode == 'both') {
+                                       // handle unicode chars possibly left out
+                                       var unicodeMatches = value.match(unicodeRE);
+                                       if(unicodeMatches) {
+                                         for(var u=0; u<unicodeMatches.length; u++) {
+                                            value = value.replace( unicodeMatches[u], unescapeUnicode(unicodeMatches[u]));
+                                         }
+                                       }
+                                       // add to map
+                                       $.i18n.map[name] = value;
+                                   }
+                                   
+                                   /** Mode: bundle keys as vars/functions */
+                                   if(mode == 'vars' || mode == 'both') {
+                                       // make sure namespaced key exists (eg, 'some.key') 
+                                       checkKeyNamespace(name);
+                                       
+                                       // value with variable substitutions
+                                       if(regPlaceHolder.test(value)) {
+                                           var parts = value.split(regPlaceHolder);
+                                           // process function args
+                                           var first = true;
+                                           var fnArgs = '';
+                                           var usedArgs = [];
+                                           for(var p=0; p<parts.length; p++) {
+                                               if(regPlaceHolder.test(parts[p]) && usedArgs.indexOf(parts[p]) == -1) {
+                                                   if(!first) {fnArgs += ',';}
+                                                   fnArgs += parts[p].replace(regRepPlaceHolder, 'v$1');
+                                                   usedArgs.push(parts[p]);
+                                                   first = false;
+                                               }
+                                           }
+                                           parsed += name + '=function(' + fnArgs + '){';
+                                           // process function body
+                                           var fnExpr = '"' + value.replace(regRepPlaceHolder, '"+v$1+"') + '"';
+                                           parsed += 'return ' + fnExpr + ';' + '};';
+                                           
+                                       // simple value
+                                       }else{
+                                           parsed += name+'="'+value+'";';
+                                       }
+                                   }
+                               }
                            }
                        }
-                       parsed += name + '=function(' + fnArgs + '){';
-                       // process function body
-                       var fnExpr = '"' + value.replace(regRepPlaceHolder, '"+v$1+"') + '"';
-                       parsed += 'return ' + fnExpr + ';' + '};';
-                       
-                   // simple value
-                   }else{
-                       parsed += name+'="'+value+'";';
+                       eval(parsed);
                    }
-               } // END: Mode: bundle keys as vars/functions
-           } // END: if(pair.length > 0)
-       } // END: skip comments
-   }
-   eval(parsed);
+    });
 }
 
 /** Make sure namespace exists (for keys with dots in name) */
 function checkKeyNamespace(key) {
-	var regDot = /\./;
+	var regDot = /\./g;
 	if(regDot.test(key)) {
 		var fullname = '';
 		var names = key.split( /\./ );
@@ -236,101 +225,4 @@ function unescapeUnicode(str) {
   return unescaped;
 }
 
-/* Cross-Browser Split 1.0.1
-(c) Steven Levithan <stevenlevithan.com>; MIT License
-An ECMA-compliant, uniform cross-browser split method */
-var cbSplit;
-// avoid running twice, which would break `cbSplit._nativeSplit`'s reference to the native `split`
-if (!cbSplit) {    
-  cbSplit = function(str, separator, limit) {
-      // if `separator` is not a regex, use the native `split`
-      if (Object.prototype.toString.call(separator) !== "[object RegExp]") {
-        if(typeof cbSplit._nativeSplit == "undefined")
-          return str.split(separator, limit);
-        else
-          return cbSplit._nativeSplit.call(str, separator, limit);
-      }
-  
-      var output = [],
-          lastLastIndex = 0,
-          flags = (separator.ignoreCase ? "i" : "") +
-                  (separator.multiline  ? "m" : "") +
-                  (separator.sticky     ? "y" : ""),
-          separator = RegExp(separator.source, flags + "g"), // make `global` and avoid `lastIndex` issues by working with a copy
-          separator2, match, lastIndex, lastLength;
-  
-      str = str + ""; // type conversion
-      if (!cbSplit._compliantExecNpcg) {
-          separator2 = RegExp("^" + separator.source + "$(?!\\s)", flags); // doesn't need /g or /y, but they don't hurt
-      }
-  
-      /* behavior for `limit`: if it's...
-      - `undefined`: no limit.
-      - `NaN` or zero: return an empty array.
-      - a positive number: use `Math.floor(limit)`.
-      - a negative number: no limit.
-      - other: type-convert, then use the above rules. */
-      if (limit === undefined || +limit < 0) {
-          limit = Infinity;
-      } else {
-          limit = Math.floor(+limit);
-          if (!limit) {
-              return [];
-          }
-      }
-  
-      while (match = separator.exec(str)) {
-          lastIndex = match.index + match[0].length; // `separator.lastIndex` is not reliable cross-browser
-  
-          if (lastIndex > lastLastIndex) {
-              output.push(str.slice(lastLastIndex, match.index));
-  
-              // fix browsers whose `exec` methods don't consistently return `undefined` for nonparticipating capturing groups
-              if (!cbSplit._compliantExecNpcg && match.length > 1) {
-                  match[0].replace(separator2, function () {
-                      for (var i = 1; i < arguments.length - 2; i++) {
-                          if (arguments[i] === undefined) {
-                              match[i] = undefined;
-                          }
-                      }
-                  });
-              }
-  
-              if (match.length > 1 && match.index < str.length) {
-                  Array.prototype.push.apply(output, match.slice(1));
-              }
-  
-              lastLength = match[0].length;
-              lastLastIndex = lastIndex;
-  
-              if (output.length >= limit) {
-                  break;
-              }
-          }
-  
-          if (separator.lastIndex === match.index) {
-              separator.lastIndex++; // avoid an infinite loop
-          }
-      }
-  
-      if (lastLastIndex === str.length) {
-          if (lastLength || !separator.test("")) {
-              output.push("");
-          }
-      } else {
-          output.push(str.slice(lastLastIndex));
-      }
-  
-      return output.length > limit ? output.slice(0, limit) : output;
-  };
-  
-  cbSplit._compliantExecNpcg = /()??/.exec("")[1] === undefined; // NPCG: nonparticipating capturing group
-  cbSplit._nativeSplit = String.prototype.split;
-
-} // end `if (!cbSplit)`
-String.prototype.split = function (separator, limit) {
-    return cbSplit(this, separator, limit);
-};
-
 })(jQuery);
-                
